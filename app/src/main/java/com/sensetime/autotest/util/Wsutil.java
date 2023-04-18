460bc35477a3ccda2df1a4eac27c74dd32aba0ab
caiyuting <cy_2020@outlook.com>
2023-04-18 17:40:29 +0800

    modify

--- app/src/main/java/com/sensetime/autotest/util/Wsutil.java
+++ app/src/main/java/com/sensetime/autotest/util/Wsutil.java
@@ -4,7 +4,10 @@ public class Wsutil {
 
     public static volatile String devicesID = "";
 
-    public static String ws = "ws://savvcenter.sensetime.com:10000/ws?type=AndroidPhone&sid=";
+//    public static String ws = "ws://savvcenter.sensetime.com:10000/ws?type=AndroidPhoneNio&sid=";
+
+    //nio项目启动此
+    public static String ws = "ws://savvcenter.sensetime.com:10000/ws?type=AndroidPhoneNio&sid=";
 //    public static String ws = "ws://10.166.132.5:10000/ws?type=AndroidPhone&sid=";
 
 
